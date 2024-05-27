ec8939aae65e78700718bd6bc000c8aa37c01b08
HalaMzobe <HalaMzobe@HALAMZ>
2024-05-27 21:46:24 +0200

    Updated ViewRecipe page design
    1. Updated ViewRecipe page

--- ViewRecipe.xaml.cs
+++ ViewRecipe.xaml.cs
@@ -55,5 +55,49 @@ namespace PROG6221_FINAL
         {
 
         }
+
+        private async void btn_viewSelected_Recipe_Click(object sender, RoutedEventArgs e)
+        {
+            btn_viewSelected_Recipe.Background = Brushes.Green;
+            await Delayed1SecMethod(); // Delay for 1000 milliseconds (1 second)
+            btn_viewSelected_Recipe.Background = Brushes.Transparent;
+        }
+
+        static async Task Delayed1SecMethod()
+        {
+            await Task.Delay(100); // Delay for 1000 milliseconds (1 second)
+        }
+
+        private void btn_recipeScale_reset_Click(object sender, RoutedEventArgs e)
+        {
+            btn_recipeScale_reset.Background = Brushes.SeaGreen;
+            btn_recipeScale_half.Background = Brushes.White;
+            btn_recipeScale_double.Background = Brushes.White;
+            btn_recipeScale_triple.Background = Brushes.White;
+        }
+
+        private void btn_recipeScale_half_Click(object sender, RoutedEventArgs e)
+        {
+            btn_recipeScale_reset.Background = Brushes.White;
+            btn_recipeScale_half.Background = Brushes.SeaGreen;
+            btn_recipeScale_double.Background = Brushes.White;
+            btn_recipeScale_triple.Background = Brushes.White;
+        }
+
+        private void btn_recipeScale_double_Click(object sender, RoutedEventArgs e)
+        {
+            btn_recipeScale_reset.Background = Brushes.White;
+            btn_recipeScale_half.Background = Brushes.White;
+            btn_recipeScale_double.Background = Brushes.SeaGreen;
+            btn_recipeScale_triple.Background = Brushes.White;
+        }
+
+        private void btn_recipeScale_triple_Click(object sender, RoutedEventArgs e)
+        {
+            btn_recipeScale_reset.Background = Brushes.White;
+            btn_recipeScale_half.Background = Brushes.White;
+            btn_recipeScale_double.Background = Brushes.White;
+            btn_recipeScale_triple.Background = Brushes.SeaGreen;
+        }
     }
 }
